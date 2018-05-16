25cc89bc78f6ac1d8a6dc1de5515bca46eb22c87
Михаил <mrtgorsv@gmail.com>
2018-05-16 21:31:58 +0700

    Update answer logic for TestPart

--- AdvancedTest/ViewModels/Test/TestPartViewModel.cs
+++ AdvancedTest/ViewModels/Test/TestPartViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Media.Imaging;
 using AdvancedTest.Data.Enum;
 using AdvancedTest.ViewModel;
@@ -10,8 +11,16 @@ namespace AdvancedTest.ViewModels.Test
     {
         private BitmapImage _testText;
         private string _testPath;
+        private string _currentAnswer;
+
         public TestPartViewModel()
         {
+        }
+
+
+        public bool IsValid
+        {
+            get { return string.Compare(CorrectAnswer, CurrentAnswer, StringComparison.InvariantCulture) == 0; }
         }
 
         public string TextPath
@@ -37,6 +46,18 @@ namespace AdvancedTest.ViewModels.Test
         public TestViewModel CurrentTest { get; set; }
 
         public TestPartType TestPartType { get; set; }
+
+        public string CurrentAnswer
+        {
+            get { return _currentAnswer; }
+            set
+            {
+                _currentAnswer = value;
+                OnPropertyChanged(nameof(CorrectAnswer));
+            }
+        }
+
+        public string CorrectAnswer { get; set; }
 
         public ObservableCollection<AnswerViewModel> Answers { get; set; }
     }
